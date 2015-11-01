fde3da7e77efe19becde5f51c1d4026bc0232352
LeaYeh <smile2140@gmail.com>
2015-11-01 17:04:32 +0800

    implement log transformation

--- image_processing/proj_2/spatial_enhancement.py
+++ image_processing/proj_2/spatial_enhancement.py
@@ -21,6 +21,66 @@ def show_histogram(img):
   return 0
 
 
+def log_transform(img, c):
+  height, width = (img.shape[0], img.shape[1])
+  blank_image = np.zeros((height, width, 1), np.uint8)
+  tmp_image = np.zeros((height, width))
+  show_histogram(img)
+
+  if c == -1:
+    max_val = 0
+
+    for row, col in product(range(0, height), range(0, width)):
+      gray_val = math.log10(1 + img[row, col])
+      if gray_val > max_val:
+        max_val = gray_val
+      tmp_image[row, col] = math.log10(1 + img[row, col])
+
+    for row, col in product(range(0, height), range(0, width)):
+      blank_image[row, col] = tmp_image[row, col] * 255 / max_val
+
+  else:
+    for row, col in product(range(0, height), range(0, width)):
+      blank_image[row, col] = c * math.log10(1 + img[row, col])
+  #blank_image =list(
+  #                  map(
+  #                    lambda x: x * 255 / (max_val - min_val), 
+  #                    blank_image.ravel()
+  #                  )
+  #                 ).reshape(height, width)
+  cv.imshow('log res', blank_image)
+  cv.waitKey(0)
+  cv.destroyAllWindows()
+  return blank_image
+
+
+def powerlaw_transform(img, c, gamma):
+  height, width = (img.shape[0], img.shape[1])
+  blank_image = np.zeros((height, width, 1), np.uint8)
+
+  if c != -1:
+    for row, col in product(range(0, height), range(0, width)):
+      blank_image[row, col] = c * (img[row, col])**gamma
+
+  else:
+    max_val = 0
+    tmp_image = np.zeros((height, width))
+
+    for row, col in product(range(0, height), range(0, width)):
+      tmp_image[row, col] = (img[row, col])**gamma
+      if tmp_image[row, col] > max_val:
+        max_val = tmp_image[row, col]
+
+    for row, col in product(range(0, height), range(0, width)):
+      blank_image[row, col] = tmp_image[row, col] * 255 / max_val
+  
+  cv.imshow('powerlaw res', blank_image)
+  cv.waitKey(0)
+  cv.destroyAllWindows()
+
+  return blank_image
+
+
 if __name__ == '__main__':
   # Load an color image in grayscale
   img = cv.imread("images/Fig0308(a)(fractured_spine).tif", 0)
