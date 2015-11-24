49d761998dfe6e35baddf667a3261c43348194b7
LeaYeh <smile2140@gmail.com>
2015-11-24 18:26:05 +0800

    initial main file and required files

---
+++ image_processing/proj_3/frequency_transform.py
@@ -0,0 +1,30 @@
+import cv2 as cv
+import numpy as np
+import math
+import sys
+from itertools import product
+import matplotlib
+matplotlib.use("Qt4Agg")
+import matplotlib.pyplot as plt
+from dependence.spatial_enhancement import show_histogram, histogram_equalize
+
+
+# Fast Fourier Transform
+def FFT():
+  return 0
+
+
+# High-Frequency-Emphasis Filtering
+def hightpass_filter():
+  return 0
+
+
+# Butterworth band reject filter
+def butterworth_filter():
+  return 0
+
+
+
+if __name__ == '__main__':
+
+
