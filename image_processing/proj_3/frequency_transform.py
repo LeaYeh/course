cc6c2cb6833d2187bc33a7d5eb5e0cfe74b93706
LeaYeh <smile2140@gmail.com>
2015-12-03 00:03:18 +0800

    implement [func]padding to let image be power of 2

--- image_processing/proj_3/frequency_transform.py
+++ image_processing/proj_3/frequency_transform.py
@@ -9,6 +9,33 @@ from dependence.spatial_enhancement import matplotlib
 from dependence.spatial_enhancement import product
 
 
+# check if a number is a power of two
+def _is_power_2(num):
+  return ( num != 0 and ((num & (num - 1)) == 0))
+
+# calc highest bit position then complete to power of 2
+def _complete_to_power_2(num):
+  return 2 ** (int(math.log(num, 2)) + 1)
+
+
+# let image be power of 2
+def _padding(img):
+  height, width = img.shape[0], img.shape[1]
+  new_height, new_width = 0, 0
+
+  if not _is_power_2(height):
+    new_height = _complete_to_power_2(height)
+  if not _is_power_2(width):
+    new_width = _complete_to_power_2(width)
+
+  img = np.pad(
+          img,
+          ((0, new_height - height), (0, new_width - width)),
+          'constant', 
+          constant_values=0
+        )
+  
+  return img
 
 
 # implement DFT
