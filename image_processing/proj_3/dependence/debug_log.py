d134ff0df1efc897974b3f87a4d70931fe1f55c9
LeaYeh <smile2140@gmail.com>
2015-12-15 20:31:28 +0800

    add time cost info into debug_log

--- image_processing/proj_3/dependence/debug_log.py
+++ image_processing/proj_3/dependence/debug_log.py
@@ -1,8 +1,12 @@
+import time 
+
 def log_msg(func):
   def with_logging(*arg, **kwargs):
     print(func.__name__ + "...", end="", flush=True)
+    t0 = time.time()
     res = func(*arg, **kwargs)
-    print("done")
+    t1 = time.time()
+    print("done  %.2g sec" % (t1 - t0))
 
     return res
 
